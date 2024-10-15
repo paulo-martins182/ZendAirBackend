1d7925a25091a01b4ef490da7223345e4acfb8c4
Paulo Martin <156706183+paulo-martin@users.noreply.github.com>
2024-10-14 22:43:55 -0300

    create basic routes

---
+++ src/routes/create-trip.ts
@@ -0,0 +1,96 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import nodemailer from "nodemailer";
+import { dayjs } from "../lib/dayjs";
+import z from "zod";
+import { prisma } from "../lib/prisma";
+import { getMailClient } from "../lib/mail";
+
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/create-trip",
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(4),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+          owner_name: z.string(),
+          owner_email: z.string().email(),
+          emails_to_invite: z.array(z.string().email()),
+        }),
+      },
+    },
+    async (request) => {
+      const { destination, ends_at, starts_at, emails_to_invite } =
+        request.body;
+
+      if (dayjs(starts_at).isBefore(new Date())) {
+        throw new Error("Starts at date must be in the future");
+      }
+
+      if (dayjs(ends_at).isBefore(starts_at)) {
+        throw new Error("Ends at date must be after starts at date");
+      }
+
+      const trip = await prisma.trip.create({
+        data: {
+          destination,
+          starts_at,
+          ends_at,
+          participants: {
+            createMany: {
+              data: [
+                {
+                  name: request.body.owner_name,
+                  email: request.body.owner_email,
+                  is_owner: true,
+                  is_confirmed: true,
+                },
+
+                ...emails_to_invite.map((email: string) => ({ email })),
+              ],
+            },
+          },
+        },
+      });
+
+      const formattedStartDate = dayjs(starts_at).format("LL");
+      const formattedEndDate = dayjs(ends_at).format("LL");
+
+      /* const confirmationLink = `${env.API_BASE_URL}/trips/${trip.id}/confirm` */
+
+      const mail = await getMailClient();
+
+      const message = await mail.sendMail({
+        from: {
+          name: "Traveler",
+          address: "sHk0H@example.com",
+        },
+        to: {
+          name: request.body.owner_name,
+          address: request.body.owner_email,
+        },
+
+        subject: "Trip created",
+        html: `
+          <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+            <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
+            <p></p>
+            <p>Para confirmar sua viagem, clique no link abaixo:</p>
+            <p></p>
+            <p>
+              <a href="#">Confirmar viagem</a>
+            </p>
+            <p></p>
+            <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
+          </div>
+        `.trim(),
+      });
+
+      console.log(nodemailer.getTestMessageUrl(message));
+
+      return { tripId: trip.id };
+    }
+  );
+}
